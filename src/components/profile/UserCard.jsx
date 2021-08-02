d4ab0da6d451112eaa3692337331a91c2c293eb8
지현 <spy03128@naver.com>
2021-08-02 18:16:41 +0900

    🔍️ components 오류 수정

--- src/components/profile/UserCard.jsx
+++ src/components/profile/UserCard.jsx
@@ -1,30 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const UserCard = () => {
   return (
-    <div class="col-lg-3">
-      <div class="box small h-80">
-        <div class="d-flex align-items-center mb-2 card mycard">
-          <img
+    <div className="col-lg-3">
+      <div className="box small h-80">
+        <div claclassNamess="d-flex align-items-center mb-2 card mycard">
+          <image
             src="images/img_2.jpg"
-            class="img-fluid mycard-img"
+            className="img-fluid mycard-img"
             alt="Image"
           />
 
-          <div class="text">
+          <div className="text">
             <h3>2018038066</h3>
             <h2>박지현</h2>
           </div>
-          <div class="role">
-            <a href="#" class="category">
+          <div className="role">
+            <Link to="#" className="category">
               학생
-            </a>
-            <a href="#" class="category">
+            </Link>
+            <Link to="#" className="category">
               튜터
-            </a>
-            <a href="#" class="category">
+            </Link>
+            <Link to="#" className="category">
               연구실인턴십
-            </a>
+            </Link>
           </div>
         </div>
       </div>
--- src/components/profile/UserInfo.jsx
+++ src/components/profile/UserInfo.jsx
@@ -2,63 +2,63 @@ import React from "react";
 
 const UserInfo = () => {
   return (
-    <div class="col-lg-9">
-      <div class="section-title">
+    <div className="col-lg-9">
+      <div className="section-title">
         <p>정보수정</p>
       </div>
-      <div class="box h-100">
-        <div class="d-flex align-items-center">
-          <div class="section">
+      <div className="box h-100">
+        <div className="d-flex align-items-center">
+          <div className="section">
             <form>
-              <div class="col-md-12">
+              <div className="col-md-12">
                 <p>학번</p>
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   placeholder="학번을 입력하세요"
                 />
               </div>
 
-              <div class="col-md-12">
+              <div className="col-md-12">
                 <p>이름</p>
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   placeholder="이름을 입력하세요"
                 />
               </div>
-              <div class="col-md-12">
+              <div className="col-md-12">
                 <p>이메일</p>
                 <input
                   type="email"
-                  class="form-control"
+                  className="form-control"
                   placeholder="이메일을 입력하세요"
                 />
               </div>
-              <div class="col-md-12">
+              <div className="col-md-12">
                 <p>휴대폰번호</p>
                 <input
                   type="number"
-                  class="form-control"
+                  className="form-control"
                   placeholder="휴대폰번호를 입력하세요"
                 />
               </div>
-              <div class="col-md-12">
+              <div className="col-md-12">
                 <p>소속</p>
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   placeholder="소속을 입력하세요"
                 />
               </div>
             </form>
           </div>
         </div>
-        <div class="button">
-          <button type="submit" class="sub-button">
+        <div className="button">
+          <button type="submit" className="sub-button">
             초기화
           </button>
-          <button type="submit" class="sub-button">
+          <button type="submit" className="sub-button">
             저장
           </button>
         </div>
