edae23fc6cb8fab93ce30431e445ca82bf75978d
지현 <spy03128@naver.com>
2021-08-08 14:02:50 +0900

    :wrench: usercard img 수정

--- src/components/noticelist/UserCard.jsx
+++ src/components/noticelist/UserCard.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import profileimg from "../../img/profile.png";
 
 const UserCard = () => {
   return (
     <div className="col-lg-3">
       <div className="box small h-80">
         <div className="d-flex align-items-center mb-2 card mycard">
-          <image
-            src="images/img_2.jpg"
-            className="img-fluid mycard-img"
-            alt="Image"
-          />
+          <img src={profileimg} className="img-fluid mycard-img" alt="Image" />
 
           <div className="text">
             <h3>2018038066</h3>
--- src/components/profile/UserCard.jsx
+++ src/components/profile/UserCard.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import profileimg from "../../img/profile.png";
 
 const UserCard = ({ userProfile }) => {
-  console.log(userProfile)
+  console.log(userProfile);
   return (
     <div className="col-lg-3">
       <div className="box small h-80">
         <div className="d-flex align-items-center mb-2 card mycard">
-          <image
-            src="images/img_2.jpg"
-            className="img-fluid mycard-img"
-            alt="Image"
-          />
+          <img src={profileimg} className="img-fluid mycard-img" alt="Image" />
 
           <div className="text">
             <h3>{userProfile.userId}</h3>
