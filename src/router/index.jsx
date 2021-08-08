4092d8c6caec6fbbd04ed1b4f547c91fe913e27d
지현 <spy03128@naver.com>
2021-08-08 13:53:13 +0900

    :heavy_plus_sign: notice data 추가

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -18,6 +18,49 @@ const SotongRouter = () =>
       role: "ADMIN",
       //받아온거 넣기
     };
+    const noticeData = {
+      content: [
+        {
+          id: 1,
+          name: "제목1",
+          swurl:
+            "https://sw7up.cbnu.ac.kr/community/notice/6107829bd2329b2b74c84c01",
+          tag1: "1",
+          tag2: "2",
+          tag3: "3",
+          uploadDay: "2021-08-07T08:57:27.571+00:00",
+          startDay: "2021-08-12",
+          destDay: "2021-08-31",
+          viewCount: 0,
+          memberDao: null,
+        },
+      ],
+      pageable: {
+        sort: {
+          sorted: true,
+          unsorted: false,
+          empty: false,
+        },
+        pageNumber: 0,
+        pageSize: 10,
+        offset: 0,
+        paged: true,
+        unpaged: false,
+      },
+      last: true,
+      totalPages: 1,
+      totalElements: 1,
+      first: true,
+      size: 10,
+      number: 0,
+      sort: {
+        sorted: true,
+        unsorted: false,
+        empty: false,
+      },
+      numberOfElements: 1,
+      empty: false,
+    };
 
     return (
       <>
@@ -45,6 +88,7 @@ const SotongRouter = () =>
               <NoticeListRouter
                 security={["ADMIN", "GUEST", "USER"]}
                 userInfo={userInfo}
+                noticeData={noticeData}
               ></NoticeListRouter>
             </Route>
           </Switch>
