d815fb61ffd4084c0204e8643d15ad2d4d6eb1a2
euncherry <csmo2642@naver.com>
2021-08-08 17:35:51 +0900

    :goal_net: catch error detail page

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,6 +1,6 @@
 //NOTE HOME CONTENT
 import React from "react"
-import get_noticelist from "../../service/api/get/get_noticelist"
+import get_noticelist from "../../service/api/get/get_noticedetail"
 
 const HomeContent = () => {
 
@@ -8,20 +8,14 @@ const HomeContent = () => {
         get_noticelist(0)
             .then((res) => {
                 console.log(res)
-            })
-            .catch((err) => console.log(err))
-    }
-    const getnoticeList1 = () => {
-        get_noticelist(1)
-            .then((res) => {
-                console.log(res)
+                console.log(res.content)
             })
             .catch((err) => console.log(err))
     }
 
+
     console.log("getNoticeList")
     getnoticeList0()
-    getnoticeList1()
     return (
         <>
             {/* home 관련 component */}
---
+++ src/service/api/get/get_noticedetail.js
@@ -0,0 +1,34 @@
+import _ from "../../../config/env"
+
+/**
+ * @description 공지사항 table Data 받아오기
+ * @method GET
+ * @request @headers  SoTong-token
+ * @response
+ */
+
+
+const get_noticelist = (noticeId)=>{
+ 
+    return fetch(_.SERVER_URL + "/notice/detail/"+ Number(noticeId), {
+            method: 'GET',
+            headers : {
+                'Authorization': "Bearer " + localStorage.getItem("SoTong-token")
+            },
+        }).then((res)=> {
+            if(res.status===500) throw Promise.resolve({errorCode: 500, errorName: "Server error"})
+            if(!res.ok) throw res.json()
+            console.log(res)
+            return res.json()
+        })
+        .catch(async(error)=>{
+            let err = await error.then()
+            console.log(err)
+            console.log("Error from  get_noticelist\n"+err.message+"\n success : "+err.success)
+            throw err;
+        })
+     //}
+     
+ }
+
+ export default get_noticelist
