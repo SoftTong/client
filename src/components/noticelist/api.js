13cc583894791439015608dfe4f8e8d69b6ef79f
지현 <spy03128@naver.com>
2021-08-08 16:09:24 +0900

    :hammer: noticelist map으로 수정

--- src/components/noticelist/api.js
+++ src/components/noticelist/api.js
@@ -1,45 +1,39 @@
 export const getManageApplication = (userId) => {
   const result = [
     {
-      number: "1",
+      number: 1,
       title: "2021학년도 튜터 모집",
       person: "박지현",
-      click: 12,
       date: "2021-07-31",
     },
     {
-      number: "2",
+      number: 2,
       title: "펌웨어 프로그래밍 조교 모집",
       person: "신치용",
-      click: 0,
       date: "2022-11-31",
     },
     {
-      number: "3",
+      number: 3,
       title: "SW중심사업단 집중 특강",
       person: "조광식",
-      click: 100,
       date: "2023-01-21",
     },
     {
-      number: "4",
+      number: 4,
       title: "SW중심사업단 집중 특강",
       person: "이채은",
-      click: 43,
       date: "2028-12-21",
     },
     {
-      number: "5",
+      number: 5,
       title: "SW중심사업단 집중 특강",
       person: "정상록",
-      click: 102,
       date: "2022-09-01",
     },
     {
-      number: "6",
+      number: 6,
       title: "SW중심사업단 집중 특강",
       person: "장동혁",
-      click: 230,
       date: "2023-03-16",
     },
   ];
