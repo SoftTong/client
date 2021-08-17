b702f0ebba17b5ef3fd8c540ccfb628470b89295
지현 <spy03128@naver.com>
2021-08-17 18:10:31 +0900

    :memo: plus detail page content

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -17,37 +17,7 @@ const HomeContent = () => {
 
   return (
     <>
-      <div className="col-lg-12 userinfotext">
-        <div className="section-title">
-          <p>프로그램 신청</p>
-        </div>
-
-        <div className="box">
-          <div className="align-items-center">
-            <h2>2021학년도 2학기 튜티모집</h2>
-            <div className="section">
-              <iframe
-                src="https://sw7up.cbnu.ac.kr/community/notice/61108786d2329b2b74c84c02"
-                width="100%"
-                height="650px"
-              ></iframe>
-            </div>
-            <h2>첨부파일 제출하기</h2>
-            <div className="section submitform">
-              <div className="detailbox">
-                <div className="detail">
-                  <p>이름</p>
-                  <input type="text"></input>
-                </div>
-              </div>
-            </div>
-            <div className="buttonsection">
-              <button className="sub-button detail-button">신청하기</button>
-              <button className="sub-button detail-button">돌아가기</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <p>home page</p>
     </>
   );
 };
--- src/components/noticelist/detailTest.jsx
+++ src/components/noticelist/detailTest.jsx
@@ -1,16 +1,54 @@
-import React from "react"
-
-
-
-
+import React from "react";
 
 const Test = ({ detailNoticeData }) => {
-    console.log("DetailTest")
-    console.log(detailNoticeData)
-    return (
-        <div>{detailNoticeData.title}</div>
-    )
-}
+  console.log("DetailTest");
+  console.log(detailNoticeData);
+  return (
+    <div className="box">
+      <div className="align-items-center">
+        <h2>{detailNoticeData.title}</h2>
 
+        {detailNoticeData.tag1 ? (
+          <span className="tagcategory detailtag">{detailNoticeData.tag1}</span>
+        ) : null}
+        {detailNoticeData.tag2 ? (
+          <span className="tagcategory detailtag">{detailNoticeData.tag2}</span>
+        ) : null}
+        {detailNoticeData.tag3 ? (
+          <span className="tagcategory detailtag">{detailNoticeData.tag3}</span>
+        ) : null}
+        <div className="date">
+          <p>작성일 : {detailNoticeData.startDay}</p>
+          <p>마감일 : {detailNoticeData.destDay}</p>
+        </div>
+
+        <hr></hr>
+        <div className="section">
+          <iframe
+            src={detailNoticeData.swurl}
+            width="100%"
+            height="650px"
+          ></iframe>
+        </div>
+        {/* <h2>신청서 작성하기</h2>
+        <div className="section submitform">
+          <div className="detailbox">
+            <div className="detail">
+              <p>이름</p>
+              <input type="text"></input>
+            </div>
+          </div>
+        </div> */}
+        <div className="buttonsection">
+          <button className="sub-button detail-button">신청서 작성하기</button>
+          <button className="sub-button detail-button">
+            첨부파일 제출하기
+          </button>
+          <button className="sub-button detail-button">돌아가기</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Test;
